dce9c3e7dd8dc6672dc1f558205165e2d2d6d11e
liushuai <liushuai@kuaishou.com>
2018-03-26 16:27:15 +0800

    add list branch

---
+++ git-list-branch.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+FOLDER=$1
+DEPTH=$2
+
+if (test -z "$FOLDER"); then
+    #echo "Usage: sh git-list-branch.sh @git-parent-folder [@depth]"
+    #echo "use current folder:`pwd`"
+    FOLDER=`pwd`
+fi
+
+if (test -z "$DEPTH"); then
+    DEPTH=2
+    #echo "set depth:$DEPTH"
+fi
+
+function git_list_branch_() {
+    local target=$1
+    local depth=$2
+    
+    if (test -z "$target") || (test -z "$depth"); then
+        echo "Usage: git_branch @target-folder @seek-depth"
+    fi
+
+    cd $target
+    if [ -d ".git/" ];then
+        local count=`git branch | wc -l`
+        if [ $count -gt 1 ];then
+            echo "repository: `pwd | awk -F "$origin_work_path" '{print $2}' | sed -e 's/^\///g'`"
+            git branch
+            echo ""
+        fi
+    else
+        if [ $depth -gt 0 ];then
+            for file in `ls $target`
+            do
+                cd $target
+                if [ -d $file ];then
+                    git_list_branch_ "$target/$file" "$(($depth-1))"
+                fi
+            done
+        fi
+    fi
+}
+
+origin_work_path=`pwd`
+
+git_list_branch_ "$FOLDER" "$DEPTH"
+
+cd $origin_work_path
+
+exit 0
